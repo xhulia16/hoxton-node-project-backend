74ed59cd0e34868fb6f6f0c171eac48bdf872623
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-26 22:59:09 +0200

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express'
 import cors from 'cors'
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 
 const prisma  = new PrismaClient()
-
-
 
 
 const app = express()
@@ -18,10 +16,6 @@ app.get('/posts' , async (req, res)=>{
    const posts =  await prisma.post.findMany({include:{user:true, comments:true, likes:true}})
    res.send(posts)
 })
-
-
-
-
 
 
 
