dc68a00e474395b87049f8a47f10a5b83fe36bcc
Fevronia Gjini <fevronigjini1@gmail.com>
2022-09-26 22:20:45 +0200

    get a specific post

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,44 @@
-import express from 'express'
-import cors from 'cors'
-import {PrismaClient} from '@prisma/client'
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-const prisma  = new PrismaClient()
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+const port = 5126;
 
+//  get all posts
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany({
+      include: { user: true, comments: true, likes: true },
+    });
+    res.send(posts);
+  } catch (error) {
+    // @ts-ignore
+    res.status(400).send({ error: error.message });
+  }
+});
 
+//  get a specific post
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const postId = Number(req.params.id);
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: { user: true, likes: true, comments: true },
+    });
+    res.send(post);
+  } catch (error) {
+    // @ts-ignore
+    res.status(400).send({ error: error.message });
+  }
+});
 
-const port = 5126
-
-app.get('/posts' , async (req, res)=>{
-   try{
-    const posts =  await prisma.post.findMany({include:{user:true, comments:true, likes:true}})
-     res.send(posts)
-    } catch (error){
-        // @ts-ignore
-        res.status(400).send({error:error.message})
-    }
-})
-
-
-
-
-
-
-
-
-app.listen(port, ()=>{
-    console.log(`yay : http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`yay : http://localhost:${port}`);
+});
