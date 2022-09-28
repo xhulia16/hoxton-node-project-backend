a58de615d414bd8a5cd8961e6338211b6b6acaeb
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-28 10:42:46 +0200

    changed passwords

---
+++ prisma/migrations/20220928072834_password_obligatory/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Made the column `password` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "image" TEXT,
+    "password" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("email", "id", "image", "name", "password") SELECT "email", "id", "image", "name", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
